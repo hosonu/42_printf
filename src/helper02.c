0b6e60495892738dd983ff50d9b60c1eca0488e0
hosonu <hosonoyuki07@gmail.com>
2023-10-24 03:02:38 +0900

    fix put_sign

--- src/helper02.c
+++ src/helper02.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   helper02.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hoyuki <hoyuki@student.42.fr>              +#+  +:+       +#+        */
+/*   By: hosonu <hosonu@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/21 15:32:31 by hoyuki            #+#    #+#             */
-/*   Updated: 2023/10/23 20:38:41 by hoyuki           ###   ########.fr       */
+/*   Updated: 2023/10/23 22:39:05 by hosonu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,15 +30,25 @@ void	get_digit(size_t num, t_list *format)
 	}
 }
 
-void	put_after_sign(t_list *format, int num)
+void	put_sign(t_list *format, int num, bool time)
 {
-	if (format->plus == 1 && format->is_minus == 0 && format->precision == 1)
-		print_c('+', format);
-	if (num < 0 && format->zero == 1 && format->is_minus == 1
-		&& format->precision == 1)
-		format->cnt += write(1, "-", 1);
-	if (num < 0 && format->zero == 0 && format->is_minus == 1)
-		format->cnt += write(1, "-", 1);
+	if(time == 0)
+	{
+		if (num < 0 && format->zero == 1 && format->precision == 0)
+			format->cnt += write(1, "-", 1);
+		else if (format->plus == 1 && format->precision == 0 && num >= 0)
+			format->cnt += write(1, "+", 1);
+	}
+	else if(time == 1)
+	{
+		if (format->plus == 1 && format->is_minus == 0 && format->precision == 1)
+			print_c('+', format);
+		if (num < 0 && format->zero == 1 && format->is_minus == 1
+			&& format->precision == 1)
+			format->cnt += write(1, "-", 1);
+		if (num < 0 && format->zero == 0 && format->is_minus == 1)
+			format->cnt += write(1, "-", 1);
+	}
 }
 
 void	print_d(int num, t_list *format)
@@ -56,13 +66,10 @@ void	print_d(int num, t_list *format)
 	len = format->output_len;
 	if (format->precision == 1 && format->prec_width > format->output_len)
 		len = format->prec_width;
-	if (num < 0 && format->zero == 1 && format->precision == 0)
-		format->cnt += write(1, "-", 1);
-	if (format->plus == 1 && format->zero == 1 && format->precision == 0)
-		format->cnt += write(1, "+", 1);
+	put_sign(format, num, 0);
 	if (format->flags == 1 && format->minus == 0)
 		print_fields(len, format);
-	put_after_sign(format, num);
+	put_sign(format, num, 1);
 	if (format->precision == 1)
 		print_pwidth(format->output_len, format);
 	if (!(format->precision == 1 && nb == 0))
